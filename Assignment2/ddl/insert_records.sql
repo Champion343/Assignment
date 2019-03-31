7ccbeff0631c40c59e1753f452bf72795908a753
jackson.demaree <jackson.demaree@tamu.edu>
2019-03-31 13:17:13 -0400

    fixed special characters

--- Assignment2/ddl/insert_records.sql
+++ Assignment2/ddl/insert_records.sql
@@ -1,11 +1,10 @@
 --set pk id value for ease of use
-set escape '\';
 insert into animal VALUES (1,'cat', 'owners of the human race.');
 insert into animal VALUES (2,'dog', 'primarily eats shoes and couches.');
-insert into animal VALUES (3,'alicorn', 'mythical horse with wings and horn according to coworker\'s daughter.');
+insert into animal VALUES (3,'alicorn', 'mythical horse with wings and horn according to coworker''s daughter.');
 insert into animal VALUES (4,'zebra', 'see horse but striped.');
 insert into animal VALUES (5,'horse', 'see zebra but unstriped.');
-insert into animal VALUES (6,'honey badger', 'dangerous beast that just doesn\'t care.');
+insert into animal VALUES (6,'honey badger', 'dangerous beast that just doesn''t care.');
 insert into animal VALUES (7,'snake', 'danger noodle.');
 insert into animal VALUES (8,'seahorse', 'aquatic horse.');
 insert into animal VALUES (9,'zorse', 'see horse and zebra.');
